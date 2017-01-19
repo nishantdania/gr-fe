a5e91dcf4ef31e0996dbddc746db802a02a2688f
nishantdania <nishantdania@gmail.com>
2017-01-20 00:47:52 +0530

    added error handling on home

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var app = express()
 
 app.use(express.static(__dirname));
 
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
+app.get('/coupons', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 })
 
 app.listen(3000, function () {
--- view-models/indexVM.js
+++ view-models/indexVM.js
@@ -4,6 +4,7 @@ var IndexViewModel = function() {
     this.confirmPassword = ko.observable();
 	this.email = ko.observable();
 	this.isSignUpOn = ko.observable(true);
+	this.message = ko.observable();
 
 	this.toggleSignUp = function() {
 		this.isSignUpOn(!this.isSignUpOn());
@@ -13,14 +14,53 @@ var IndexViewModel = function() {
 		var data = {};
 		data.username = this.username;
 		data.password = this.password;
+		that = this;
 		$.ajax({
 			method:'POST',
 			url: 'http://localhost:1337/login/',
 			data: data,
 			crossDomain: true,
 			dataType: 'json',
-			success: function(returnedData) { alert("Success" + returnedData);  },
-			error: function() { alert('Failed!');  }
+			success: function(returnedData) 
+			{ 
+				localStorage.setItem('token', returnedData.token);
+				window.location.replace("http://localhost:3000/coupons");
+			},
+			error: function(returnedData) { 
+				if(typeof returnedData.responseJSON.err != Object)
+					that.message(returnedData.responseJSON.err.toString());
+				else 
+					that.message('No fields can be left empty');
+					$("#message").show().delay(5000).fadeOut();
+			}
+		});
+	};
+
+	this.signUpHandler = function() {
+		var data = {};
+		data.username = this.username;
+		data.password = this.password;
+		data.email = this.email;
+		data.confirmPassword = this.confirmPassword;
+		that = this;
+		$.ajax({
+			method:'POST',
+			url: 'http://localhost:1337/signup/',
+			data: data,
+			crossDomain: true,
+			dataType: 'json',
+			success: function(returnedData) 
+			{ 
+				localStorage.setItem('token', returnedData.token);
+				window.location = "http://localhost:3000/coupons";
+			},
+			error: function(returnedData) { 
+				if(typeof returnedData.responseJSON.err != Object)
+					that.message(returnedData.responseJSON.err.toString());
+				else 
+					that.message('No fields can be left empty');
+					$("#message").show().delay(5000).fadeOut();
+			}
 		});
 	};
 };
